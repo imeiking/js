bd0993db5715ff595b0215a22d9d0ffdf8df864a
imeiking <haohaoxuexi1@protonmail.com>
2025-11-12 20:52:18 +0800

    Update jpyy.js

--- jpyy.js
+++ jpyy.js
@@ -6,7 +6,7 @@ const UA =
 let appConfig = {
     ver: 1,
     title: '金牌影视',
-    site: $cache.get('jpyy_host') || 'https://fitcome.com',
+    site: $cache.get('jpyy_host') || 'https://www.cfkj86.com',
     tabs: [
         {
             name: '电影',
